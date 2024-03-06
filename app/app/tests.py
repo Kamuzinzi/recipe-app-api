0e575b97a7c24fd385954fa126e3040f9b69574b
Egide <egidenize@gmail.com>
2024-03-06 11:07:01 +0200

    Solved white space issues from lint

--- app/app/tests.py
+++ app/app/tests.py
@@ -11,9 +11,9 @@ class CalcTests(SimpleTestCase):
     """" Test the calc module."""
 
     def test_add_numbers(self):
-        res = calc.add(5, 6) 
+        res = calc.add(5, 6)
         self.assertEqual(res, 11)
-    
+ 
     def test_substract_numbers(self):
         """"Test substracting numbers."""
         res = calc.substract(10, 15)
