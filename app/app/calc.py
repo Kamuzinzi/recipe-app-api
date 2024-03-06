62d8c6b14778265a6efd3956e999a6482f3eca20
Egide <egidenize@gmail.com>
2024-03-06 10:58:04 +0200

    Solved white space issues from lint

--- app/app/calc.py
+++ app/app/calc.py
@@ -4,10 +4,11 @@ Calculator functions
 """
 
 
-def add(x,y):
+def add(x, y):
     """ Add x and y and return result."""
 
     return x + y
+
 
 def substract(x,y):
     """"Substract x from y and return result."""
